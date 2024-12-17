c608ada948c384f52a3ae977683fd6a07094c472
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-12-17 20:47:32 +0700

    feat: add user identity at navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,17 +1,30 @@
 import { SidebarTrigger, useSidebar } from "@/components/UI/sidebar";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { RootState } from "@/store";
+import { User2 } from "lucide-react";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
   const { open } = useSidebar();
   const { isMobile, state, openMobile, setOpenMobile } = useSidebar();
+  const user = useSelector((state: RootState) => state.auth.user);
   return (
     <div className="border-background flex h-12 !w-full items-center border-b bg-white/70 px-6 transition-all duration-300 ease-in-out">
-      <SidebarTrigger
-        className={`self-left ${open && !isMobile ? "hidden" : ""}`}
-      />
+      <SidebarTrigger className={`${open && !isMobile ? "hidden" : ""}`} />
       <p className="mx-auto self-center text-sm font-semibold capitalize">
         {openMobile ? "" : "booking event date app"}
       </p>
+      {!isMobile && (
+        <div className="flex h-full items-center self-end">
+          <User2 />
+          <p>
+            {user?.name}{" "}
+            <span className="rounded-xl bg-primary px-2 text-white">
+              {user?.role_id === 1 ? "HR" : "Vendor"}
+            </span>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
